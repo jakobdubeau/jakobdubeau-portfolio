9cab86ea43034228a1f6684fbf61f0da5a3874d2
jakobdubeau <jakobdubeau@gmail.com>
2024-09-26 14:51:16 -0400

    Added id for navbar

--- src/app/components/ProjectsSection.jsx
+++ src/app/components/ProjectsSection.jsx
@@ -54,7 +54,7 @@ const ProjectsSection = () => {
   }
 
   return (
-    <section>
+    <section id="projects">
       <h2 className="text-center text-4xl font-bold text-white mt-8 mb-8 md:mb-12">
         My Projects
       </h2>
