9595ef53c9d5496618d6c4b8ad34383dfb3ee04b
jakobdubeau <jakobdubeau@gmail.com>
2024-10-28 13:30:38 -0400

    trying to fix email section

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -1,29 +1,23 @@
-// import { EmailTemplate } from '../../../components/EmailTemplate';
-import { NextResponse } from "next/server"
+import { EmailTemplate } from '/src/app/components/email-template.tsx';
 import { Resend } from 'resend';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
-const fromEmail = process.env.FROM_EMAIL;
 
-export async function POST(req, res) {
-  const { email, subject, message } = await req.json();
-  console.log(email, subject, message);
+export async function POST() {
   try {
-    const data = await resend.emails.send({
+    const { data, error } = await resend.emails.send({
       from: fromEmail,
       to: [fromEmail, email],
       subject: subject,
-      react: (
-      <>
-        <h1>{subject}</h1>
-        <p>Thank you for submitting!</p>
-        <p>New message submitted:</p>
-        <p>{message}</p>
-      </>
-      ),
+      react: EmailTemplate({ firstName: 'John' }),
     });
-    return NextResponse.json(data);
+
+    if (error) {
+      return Response.json({ error }, { status: 500 });
+    }
+
+    return Response.json(data);
   } catch (error) {
-    return NextResponse.json({ error });
+    return Response.json({ error }, { status: 500 });
   }
 }
