4cae7d9db75fac63bedfe65922a6984916223016
jakobdubeau <jakobdubeau@gmail.com>
2025-09-09 19:12:25 -0400

    liquid glass ooooo

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -24,7 +24,7 @@ const Navbar = ({ snowEnabled, setSnowEnabled }) => {
   const [navbarOpen, setNavbarOpen] = useState(false);
 
   return (
-    <nav className="fixed top-0 left-0 right-0 z-50 bg-[#121212] bg-opacity-90">
+    <nav className="fixed top-0 left-0 right-0 z-50 backdrop-blur-md bg-[#121212]/70">
       <div className="flex container lg:py-4 flex-wrap items-center justify-between mx-auto px-4 py-2">
         <Link
           href={"/"}
